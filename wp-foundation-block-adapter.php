31ddb567ec151ebcb2463837c7f14d0ee90c031f
setcookie <set@cooki.me>
2020-11-20 13:44:41 +0100

    bump 0.0.4

--- wp-foundation-block-adapter.php
+++ wp-foundation-block-adapter.php
@@ -6,7 +6,7 @@ Description: Wordpress Foundation Block Adapter
 Author: Frank Mueller <set@cooki.me>
 Author URI: https://github.com/setcooki/
 Text Domain: wpfba
-Version: 0.0.3
+Version: 0.0.4
 */
 
 if(!defined('FOUNDATION_BLOCK_ADAPTER_DOMAIN'))
